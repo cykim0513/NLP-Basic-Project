7db5ae62facd7a38f22adbcc7ef9e5b061884bc2
ky10035 <ky100e5@gmail.com>
2024-09-30 17:38:53 +0900

    add reduce char code

--- code/utils.py
+++ code/utils.py
@@ -12,3 +12,15 @@ def only_completion(self, dataframe):
         dataframe[text_column] = dataframe[text_column].apply(clean_text)
         
     return dataframe
+
+def reduce_char(text):
+    '''
+    특수문자 줄이기
+    참고( https://f7project.tistory.com/383 )
+    '''
+    
+    text = re.sub('(\\S)\\1+', '\\1', text)    #반복되는 문자 (ㅋㅋㅋ->ㅋ, !!!->!)
+    text = re.sub('([가-힣])\\1+', '\\1', text)   #반복되는 단어 (하하 -> 하)
+    text = re.sub(r'\b(\w{1,})\1\b', r'\1', text)    #반복되는 n자리 이상 단어 (뿌듯뿌듯 -> 뿌듯)
+    
+    return text
